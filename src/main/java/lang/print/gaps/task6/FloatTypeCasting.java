d7887eccad3c4a7b7f993ce7695147a2b9f23e27
rtemirbulat <equalityisnotequality@gmail.com>
2024-01-29 18:23:05 +0600

    Commit!

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        long roundedNumber = Math.round(numberToBeRounded);
+
+        // Print the rounded number to the console
+        System.out.println(roundedNumber);
     }
 }
